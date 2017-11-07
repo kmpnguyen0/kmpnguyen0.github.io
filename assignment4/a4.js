0155451743290d2a4199a01209efa6d74e03662a
kmpnguyen0 <31444227+kmpnguyen0@users.noreply.github.com>
2017-11-07 09:29:22 -0500

    uploaded

--- assignment4/a4.js
+++ assignment4/a4.js
@@ -1,20 +1,25 @@
-var thething = document.querySelector("h6");
+var planets = "Jupiter (69,911 km / 43,441 miles) – 1,120% the size of Earth, Saturn (58,232 km / 36,184 miles) – 945% the size of Earth, Uranus (25,362 km / 15,759 miles) – 400% the size of Earth, Neptune (24,622 km / 15,299 miles) – 388% the size of Earth, Earth (6,371 km / 3,959 miles), Venus (6,052 km / 3,761 miles) – 95% the size of Earth, Mars (3,390 km / 2,460 miles) – 53% the size of Earth, Mercury (2,440 km / 1,516 miles) – 38% the size of Earth "
+console.log(planets)
 
-console.log(thething)
+nameArray.forEach(make_a_list);
+function make_a_list(el, ix) {
+	var listItem = document.createElement("li")
 
-thething.addEventListener("click", clickyfunstuff);
+	listItem.textContent = el
 
-
-function clickyfunstuff() {
-	
-	var paragraph = document.querySelector(".output p");
-
-	paragraph.innerHTML = "hello <strong>tiffany</strong>, it works";
-
-	var trigger = document.querySelector(".output p strong")
-	console.log(trigger)
-
-	trigger.addEventListener("click", addImage);
-
-	console.log("it works!!!!");
+	container.appendChild(listItem)
 }
+
+var names = document.querySelectorAll(".output ul li")
+console.log(names)
+
+
+names.forEach(changeBackground)
+
+function changeBackground(el) {
+	el.style.backgroundColor = "whit," + Math.random() + ")";
+}
+
+
+
+
---
+++ assignment4/a5.js
@@ -0,0 +1,7 @@
+var ask = prompt ('What is the Third Largest Planet in the Solar System?');
+
+alert('Nice try!')
+
+var ask = prompt ('Here your second chance, What is the Third Largest Planet in the Solar System?');
+
+alert("Close")
