8e58cb2bed15fd43b7e9328ad5ebad963552a660
nguyk336 <nguyk336@newschool.edu>
2018-04-16 22:08:15 -0400

    update

---
+++ Lab/Javascript/index.js
@@ -0,0 +1,20 @@
+//1
+var weekend = ['sleep', 'eat', 'takePhoto', 'readDammei',  'editingPhotos', 'hangOutWithFriend'];
+
+//2
+console.log(60+6, 330/5, 72-6, 11*6, 660/10, 1000-934)
+
+//3
+var group1 = ['Kristen', 'Evelin', 'Emily', 'VincentP','Emil'];
+var group2 = ['VincentG', 'Yangchen', 'Ersa', 'Khe', 'Prithi'];
+var group3 = ['Shuting', 'Yaren', 'Soyeon', 'Seowon', 'Jahyun'];
+
+//4
+var allStudentsHere = ['Shuting', 'Yaren', 'Soyeon', 'Seowon', 'Jahyun','Kristen', 'Evelin', 'Emily', 'VincentP','Emil','VincentG', 'Yangchen', 'Ersa', 'Khe', 'Prithi']
+
+//5
+var studentsMissing = [];
+console.log(studentsMissing);
+
+
+
