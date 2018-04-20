b8d7d27f4d445360a88cbd32d5fff5442dab77e2
nguyk336 <nguyk336@newschool.edu>
2018-04-19 21:31:04 -0400

    updated

---
+++ Lab/JSquiz/main.js
@@ -0,0 +1,57 @@
+// question 1
+'Nguyen Phan My Khe';
+'95';
+var firstname = ['K','H','E'];
+['chocolate',3,true];
+txt1 = "Khe";
+txt2 = "Nguyen";
+txt3 = txt1 + " " + txt2;
+number = "10"+"10";
+
+// question 2
+var firstItem = myArray[0];
+var lastItem = [myArray.length - 1]];
+
+// question 3
+var sentence = "supercalifragilisticexpialidocious";
+var n = str.length;
+
+
+// question 5
+var student = [15];
+
+if ( student == 15 ) {
+  console.log("all the students are in class");
+} else {
+  console.log("not everyone is here");
+}
+
+// question 6
+for ( var i = 0; i < firstname.length; i++ ) {
+  console.log( firstname[i]);
+}
+
+// question 7
+var date = new Date;
+var hour = date.getHours();
+
+if ( 15<=hour<=21.75 ) {
+  console.log("true");
+} 
+else {
+  console.log("false");
+}
+
+// question 9
+var food = ['milktea','steak','cold noodle'];
+var animal = ['rabbit','panda','dog'];
+var place = ['canada','vietnam','japan'];
+var pokemon = ['zenigame','umbreon','mimikyu']
+
+// question 10
+
+
+
+
+
+
