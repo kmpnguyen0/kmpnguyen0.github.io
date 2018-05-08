74377690e350f1b1eefecfcf03722bc0691419dd
nguyk336 <nguyk336@newschool.edu>
2018-05-08 11:52:33 -0400

    updated

--- Studio/Dream/song2.js
+++ Studio/Dream/song2.js
@@ -1,14 +1,22 @@
+var song1 = '                 (Intrument playing)         Sing along in 5 - 4 - 3 - 2 - 1 and Go         When you wish upon a star         Makes no difference who you are       Anything your heart desires will come to you                         If your heart is in your dream     No request is too extreme        When you wish upon a star            A s  t h e  d r e a m e r s  d o o o o o      F a t e  i s   k i n d       She brings to those who   l o v e    The sweet fulfillment of joy     t h e i r  s e c r e t  l o n g i n gggggggggg)       uuuuuuuuuuuuuuuuuuuu           Like a bolt out of the  b l u e e e      Fate steps in and sees you  t h r o u g h          When  you  wish     upon  a  star      Your   d r e a m s s s s s      c o m e e e e e e    t r u e e e e e e                             '
+
+
 //   audio
 function music() {
     var audio = document.getElementById("myAudio").autoplay;
     document.getElementById("demo").innerHTML = audio;
 }
 
-var song1 = '            Sing along in 5  -  4  -  3  -  2  -  1 and Go  Salagadoola mechicka boola. Bibbidi-bobbidi-boo! Put them together and what have you got. Bibbidi-bobbidi-boo!  Salagadoola mechicka boola Bibbidi-bobbidi-boo! It\'ll do magic, believe it or not Bibbidi-bobbidi-boo!   Now,salagadoola means "Mechicka booleroo" But the thing mabob that does the job Is bibbidi-bobbidi-boo! Salagadoola menchicka boola Bibbidi-bobbidi-boo Put them together what have you got Bibbidi-bobbidi,Bibbidi-bobbidi Bibbidi-bobbidi boo!'
-
 
 var fonts =["Bourbon","Aleo Light Italic","Broadcast Matter","Aleo Light Italic","Broadcast Matter","Chamfort Family Bold","Eunomia Bold","Eunomia Light","Eunomia","F5.6","Fairview","Ferrum","Kelson Sans Bold","Kelson Sans Light","Kitchen Sink","Oi You","Penna","Rosario Nocera Light","Rosario Nocera","Rosario Nocera Bold","Bitter","FivoSans Bold","FivoSans Medium","Fivo Sans Thin"];
-
+// for (var i = song1.length - 1; i >= 0; i--) {
+//   song1[i]
+//   /*creating a .letter element for each letter*/
+//   var letter = $('<h1></h1>');
+//   $(letter).text(song1[i]);
+//   $('body').append(letter)
+//   // console.log(song1[])
+// }
 
 var font = "Bourbon"
 
@@ -18,6 +26,8 @@ $(window).on('click', function(){
 /* change fonts */
 var randomIndex = Math.floor((Math.random() * fonts.length - 1) + 0);
 font = fonts[randomIndex];
+
+
 })
 
 
@@ -50,12 +60,12 @@ var i = 0, l = song1.length;
 
   //window.scrollBy(30,0)
   if(i > 10){
-    $('html, body').animate({scrollLeft: $("#" + (i - 1)).position().left + 200}, 90)
+    $('html, body').animate({scrollLeft: $("#" + (i - 1)).position().left - 300}, 100)
   }
   
   $('.letters').css('width', $(window).width() + (200 * i))
 
-  if (i % 16 == 0 && (i != 0)) {
+  if (i % 20 == 0) {
     var line = $('<div></div>');
     $(line).addClass('bar');
     $('.letters').append(line) 
@@ -65,11 +75,20 @@ var i = 0, l = song1.length;
 /* if the mouse moves, show letter where mouse is*/
   var moved = false;
   $(document).mousemove(function(event) {
+        // currentMousePos.x = event.pageX;
+        // currentMousePos.y = event.pageY;
+        // $('.letter').css('margin-left','-40%');
+        // if (moved == false) {
+        //   $("#" + (i - 1)).css('top', event.pageY)
+        //   moved = true;
+        // }
+
         Y = map(event.pageY, 0, $(window).height(), 100, 480 )
+        //Y = event.pageY - 50
   });
   $("#" + (i)).css('font-family', font)
   if(++i < l){
-    setTimeout(iterator, 85); //SPEED in milliseconds
+    setTimeout(iterator, 130); //SPEED in milliseconds
   } else{
 
   }
@@ -93,7 +112,7 @@ var i = 0, l = song1.length;
 // });
 
 
-$(song1).on('ended', function(){
-    $('.result').show('ended');
-});
+
+
+
 
